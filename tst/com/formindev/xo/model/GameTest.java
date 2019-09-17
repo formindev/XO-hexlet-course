921ff086f81173d324375e3b8cf3db08a6aa2012
formindev <formindeve@gmail.com>
2019-09-17 20:05:21 +0400

    GameTest - created

---
+++ tst/com/formindev/xo/model/GameTest.java
@@ -0,0 +1,46 @@
+package com.formindev.xo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Test
+    public void getName() {
+
+        final String inputName = "XO";
+
+        final Game game = new Game(inputName, null, null);
+
+        final String actualName = game.getName();
+
+        assertEquals(inputName, actualName);
+
+    }
+
+    @Test
+    public void getPlayers() {
+
+        final Player[] inputPlayers = new Player[] {new Player("Greg", Figure.O), new Player("Bob", Figure.X)};
+
+        final Game game = new Game(null, inputPlayers, null);
+
+        final Player[] actualPlayers= game.getPlayers();
+
+        assertArrayEquals(inputPlayers, actualPlayers);
+
+    }
+
+    @Test
+    public void getField() {
+
+        final Field inputField = new Field();
+
+        final Game game = new Game(null, null, inputField);
+
+        final Field actualField = game.getField();
+
+        assertEquals(inputField, actualField);
+    }
+}
