66cca670208e2ba19cdded892107c23fa4e9d1c7
formindev <formindeve@gmail.com>
2019-09-17 22:52:56 +0400

    MoveController - created

---
+++ src/com/formindev/xo/controller/MoveController.java
@@ -0,0 +1,12 @@
+package com.formindev.xo.controller;
+
+import com.formindev.xo.model.Field;
+import com.formindev.xo.model.Figure;
+
+import java.awt.*;
+
+public class MoveController {
+    public void applyFigure(Point point, Field field, Figure figure) {
+
+    }
+}
