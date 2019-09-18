e6db8d9db74145ce4ed202187ea93b5c12b201c1
formindev <formindeve@gmail.com>
2019-09-18 15:32:04 +0400

    MoveController - add applyFigure method

--- src/com/formindev/xo/controller/MoveController.java
+++ src/com/formindev/xo/controller/MoveController.java
@@ -2,11 +2,20 @@ package com.formindev.xo.controller;
 
 import com.formindev.xo.model.Field;
 import com.formindev.xo.model.Figure;
+import com.formindev.xo.model.exceptions.AlreadyOccupiedException;
+import com.formindev.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
 public class MoveController {
-    public void applyFigure(Point point, Field field, Figure figure) {
 
+    public void applyFigure(Point point, Field field, Figure figure) throws AlreadyOccupiedException, InvalidPointException {
+
+        if (field.getFigure(point) != null) {
+           throw new AlreadyOccupiedException();
+       }
+
+        field.setFigure(point, figure);
     }
+
 }
